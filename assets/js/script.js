ec8966b10a312d183a2de77e8193c31f8902bf06
DisFinitive <dima.finick.05@bk.ru>
2022-09-09 05:41:02 +0300

    spaces to tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,91 @@
 $(function() {
 
-    var header = $("#header"),
-        introH = $("#intro").innerHeight(),
-        scrollOffset = $(window).scrollTop();
+	var header = $("#header"),
+		introH = $("#intro").innerHeight(),
+		scrollOffset = $(window).scrollTop();
 
 
-        //  Fixed Header
-        checkScroll(scrollOffset);
+		//  Fixed Header
+		checkScroll(scrollOffset);
 
-    $(window).on("scroll", function() {
+	$(window).on("scroll", function() {
 
-        scrollOffset = $(this).scrollTop();
+		scrollOffset = $(this).scrollTop();
 
-        checkScroll(scrollOffset);
+		checkScroll(scrollOffset);
 
-    });
+	});
 
-    function checkScroll(scrollOffset) {
+	function checkScroll(scrollOffset) {
 
-        if( scrollOffset >= introH ) {
-            header.addClass("header--fixed");
-        } else {
-            header.removeClass("header--fixed");
-        }
+		if( scrollOffset >= introH ) {
+			header.addClass("header--fixed");
+		} else {
+			header.removeClass("header--fixed");
+		}
 
-    }
-
-
-
-    // Smooth scroll
-
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('scroll'),
-            blockOffset = $(blockId).offset().top;
-
-        $("#nav a").removeClass("nav__link--active");
-        $this.toggleClass("nav__link--active");
-        // $this.removeClass("nav__link--active");
-
-        nav.removeClass("nav--active")
-        $("#nav_toggle").removeClass("nav-toggle--active")
+	}
 
 
-        $("html, body").animate({
-            scrollTop: blockOffset
-        }, 500);
 
-    });
+	// Smooth scroll
 
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
 
-    // Menu nav toggel
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
 
-    var nav = $("#nav")
+		$("#nav a").removeClass("nav__link--active");
+		$this.toggleClass("nav__link--active");
+		// $this.removeClass("nav__link--active");
 
-    $("#nav_toggle").on("click", function(event) {
-        event.preventDefault();
-
-        $(this).toggleClass("nav-toggle--active");
-        nav.toggleClass('nav--active');
-
-    });
-
-    // Wedo accordion
-
-    $("[data-collapse]").on("click", function(e) {
-        e.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-            $(blockId).slideToggle();
-
-        $this.toggleClass("accordion__item--active");
-    });
+		nav.removeClass("nav--active")
+		$("#nav_toggle").removeClass("nav-toggle--active")
 
 
-    // Slider 
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
 
-    $("[data-slider]").slick({
-        infinite: true,
-        fade: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+	});
+
+
+	// Menu nav toggel
+
+	var nav = $("#nav")
+
+	$("#nav_toggle").on("click", function(event) {
+		event.preventDefault();
+
+		$(this).toggleClass("nav-toggle--active");
+		nav.toggleClass('nav--active');
+
+	});
+
+	// Wedo accordion
+
+	$("[data-collapse]").on("click", function(e) {
+		e.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('collapse');
+
+			$(blockId).slideToggle();
+
+		$this.toggleClass("accordion__item--active");
+	});
+
+
+	// Slider 
+
+	$("[data-slider]").slick({
+		infinite: true,
+		fade: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
 
 
 });
